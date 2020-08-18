7cccd1a5519a07d30eb74e297b5afd6c1db866a9
Gilbert Aurel <aurel.gilbert10@gmail.com>
2020-08-18 22:53:23 +0700

    Added redis and filter logic

--- worker/tasks/fetch-github.js
+++ worker/tasks/fetch-github.js
@@ -2,16 +2,21 @@ var fetch = require('node-fetch');
 const redis = require("redis");
 const client = redis.createClient();
 
+// Redis setup
 const { promisify } = require("util");
-const getAsync = promisify(client.get).bind(client);
-const setAsync = promisify(client.get).bind(client);
+//const getAsync = promisify(client.get).bind(client);
+const setAsync = promisify(client.set).bind(client);
 
+// Api source
 const baseURL = 'https://jobs.github.com/positions.json';
 
 async function fetchGitHub(){
+
+    console.log('fetching github');
     let onPage = 0;
     const allJobs = [];
 
+    // fetch all pages
     for(;;){
         const res = await fetch(`${baseURL}?page=${onPage}`);
         const jobs = await res.json();
@@ -19,13 +24,34 @@ async function fetchGitHub(){
         if(jobs.length == 0){break;}
 
         allJobs.push(...jobs);
-        console.log(jobs.length);
+        console.log(jobs.length, 'jobs');
         onPage++;
     }
+    console.log('raw total', allJobs.length);
 
-    console.log('got', allJobs.length);
+    // filter
+    const jrJobs = allJobs.filter(job => {
+        const jobTitle = job.title.toLowerCase();
+
+        // algo logic
+        if (
+            jobTitle.includes('senior') ||
+            jobTitle.includes('manager') ||
+            jobTitle.includes('sr.') ||
+            jobTitle.includes('architect')    
+        ) {
+            return false;
+        }
+
+        return true;
+    });
+
+    console.log('filtered down to', jrJobs.length);
+
+    // Set value redis
+    const success = await setAsync('gitHub', JSON.stringify(jrJobs)); //SET REDIS
+    console.log({success});
 }
 
-fetchGitHub();
-
+//fetchGitHub();
 module.exports = fetchGitHub;
