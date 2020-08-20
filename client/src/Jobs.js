f74dc52769c14ec78b7dabe53df74773ea7a5a8b
Gilbert Aurel <aurel.gilbert10@gmail.com>
2020-08-20 11:32:41 +0700

    Added pop up page

--- client/src/Jobs.js
+++ client/src/Jobs.js
@@ -1,20 +1,81 @@
 import React from "react";
+
+// Material UI
 import Typography from '@material-ui/core/Typography';
+import MobileStepper from '@material-ui/core/MobileStepper';
+import Button from '@material-ui/core/Button';
+import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft';
+import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
  
 import Job from './Job';
+import JobModal from './JobModal';
+// import { DialogContent } from "@material-ui/core";
 
 
 export default function Jobs({jobs}){
+
+    //modal
+    const [open, setOpen] = React.useState(false);
+    const [selectedJob, selectJob] = React.useState({});
+    const handleClickOpen = () => {
+      setOpen(true);
+    };
+  
+    const handleClose = () => {
+      setOpen(false);
+    };
+
+    // Stepper state and pagination
+    const [activeStep, setActiveStep] = React.useState(0);
+    const maxSteps = Math.ceil(jobs.length / 20);
+    
+    const handleNext = () => {
+        setActiveStep((prevActiveStep) => prevActiveStep + 1);
+    };
+    
+    const handleBack = () => {
+        setActiveStep((prevActiveStep) => prevActiveStep - 1);
+    };
+    const jobsOnPage = jobs.slice(activeStep * 20, (activeStep * 20)+20);
+      
     return(
         <div className="jobs">
-            <Typography variant="h1">
+            <JobModal open={open} job={selectedJob} handleClose={handleClose}/>
+            <Typography variant="h4" component="h1">
                 Entry Level Software Jobs
             </Typography>
+            <Typography variant="h6" component="h2">
+                {jobs.length} jobs available
+            </Typography>
             {
-                jobs.map(
-                    job => <Job job={job} />
+                jobsOnPage.map(
+                    (job, i) => <Job key={1} job={job} onClick={() => {
+                        handleClickOpen()
+                        selectJob(job)
+                    }}/>
                 )
             }
+            {/* <div>
+                Page {activeStep + 1} of {maxSteps - 1}
+            </div> */}
+            <MobileStepper
+                steps={maxSteps}
+                position="static"
+                variant="text"
+                activeStep={activeStep}
+                nextButton={
+                <Button size="small" onClick={handleNext} disabled={activeStep === maxSteps - 1}>
+                    Next
+                    {<KeyboardArrowRight />}
+                </Button>
+                }
+                backButton={
+                <Button size="small" onClick={handleBack} disabled={activeStep === 0}>
+                    {<KeyboardArrowLeft />}
+                    Back
+                </Button>
+                }
+            />
         </div>
     );
 }
